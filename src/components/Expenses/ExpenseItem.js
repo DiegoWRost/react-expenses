c5aa79afed2951dfc2a4b798562d0a9283ac170a
DiegoWRost <diego.wr@gmail.com>
2021-09-27 11:20:01 -0600

    Section 05 - Renderering Lists

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -8,23 +8,17 @@ import "./ExpenseItem.css";
 const ExpenseItem = props => {
   // React Hook (Can only be called inside component functions and outside nested functions)
   // Convention is [name, setName]
-  const [title, setTitle] = useState(props.title);
-  console.log('ExpenseItem evaluated by React');
-
-  const clickHandler = () => { // Convention is to end handlers with "Handler"
-    setTitle('Updated!');
-    console.log(title);
-  };
+  // const [title, setTitle] = useState(props.title);
+  // console.log('ExpenseItem evaluated by React');
 
   return (
     // Only one root element (1 div)
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   ); // No parenthesis when executing method because JS would execute function when parsing line
 }
